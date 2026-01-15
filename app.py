376a47ac0e6986e996bf89453967c32b17198530
duyho160313 <bobbyho160313@gmail.com>
2026-01-15 19:37:46 +0700

    new code

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    hotels = [
+        {"name": "Luxury Hotel", "price": 120},
+        {"name": "Beach Resort", "price": 90},
+        {"name": "City Hotel", "price": 70}
+    ]
+    return render_template("game_web.html", hotels=hotels)
 
-@app.route('/')
-def hello():
-    return render_template("game_web.html")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
