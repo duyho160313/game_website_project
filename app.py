0fda376c880f1f9c373e8fe453e3d6c26427848b
duyho160313 <bobbyho160313@gmail.com>
2026-01-29 18:33:32 +0700

    new code

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ def home():
         {"name": "City Hotel", "price": 70}
     ]
     return render_template("game_web.html", hotels=hotels)
+@app.route('/new')
+def new_screen():
+    """Renders the destination page."""
+    return render_template('new_screen.html')
+@app.route('/screen')
+def new():
+    """Renders the destination page."""
+    return render_template('new.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
